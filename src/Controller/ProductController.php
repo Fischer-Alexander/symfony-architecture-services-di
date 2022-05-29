5d4528a8dbe576e20fb36b6a77830d48caf1521a
Alexander Fischer <fischer.online@gmx.net>
2022-05-29 19:45:03 +0200

    Creating/Configuring Services in the Container

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MessageGenerator;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,6 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    /**
+     * @param MessageGenerator $messageGenerator
+     * @return Response
+     * @Route("/products/new")
+     */
+    public function new(MessageGenerator $messageGenerator)
+    {
+        $message = $messageGenerator->getHappyMessage();
+        return new Response('The random message: '. $message);
+    }
+
     /**
      * @Route("/product", name="app_product")
      */
